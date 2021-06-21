3d89fe6e6b6e6ab2794f9ea1986ba1d2cad96042
marco <marco@marcobehler.com>
2021-06-21 16:04:49 +0200

    [webinar-prep] added war deployment

--- src/main/java/com/example/tcbuildpipelines/TcBuildPipelinesApplication.java
+++ src/main/java/com/example/tcbuildpipelines/TcBuildPipelinesApplication.java
@@ -2,9 +2,16 @@ package com.example.tcbuildpipelines;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TcBuildPipelinesApplication {
+public class TcBuildPipelinesApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TcBuildPipelinesApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TcBuildPipelinesApplication.class, args);
