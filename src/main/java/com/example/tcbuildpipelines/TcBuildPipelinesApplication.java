531fe1a67615118d21e9e4c49a23cbbd27d02fa4
marco <marco@marcobehler.com>
2021-06-22 17:42:26 +0200

    Revert "[webinar-prep] added war deployment"
    This reverts commit 3d89fe6e

--- src/main/java/com/example/tcbuildpipelines/TcBuildPipelinesApplication.java
+++ src/main/java/com/example/tcbuildpipelines/TcBuildPipelinesApplication.java
@@ -2,16 +2,9 @@ package com.example.tcbuildpipelines;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TcBuildPipelinesApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(TcBuildPipelinesApplication.class);
-    }
+public class TcBuildPipelinesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TcBuildPipelinesApplication.class, args);
